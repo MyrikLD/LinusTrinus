4b4c1b85ddc46d1013a8be87dba34c8ef54d9ee5
Myrik <myrik260138@tut.by>
2020-05-10 21:10:01 +0300

    fix include paths

--- drop_queue.py
+++ drop_queue.py
@@ -5,7 +5,7 @@ class DropQueue(queue.Queue):
     def put(self, *args, **kwargs):
         if self.full():
             try:
-                self.get()
+                self.get_nowait()
             except queue.Empty:
                 pass
         queue.Queue.put(self, *args, **kwargs)
--- samples/make.sh
+++ samples/make.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-STEAM_PATH=~/.steam/steam
-STEAMVR_PATH=$STEAM_PATH/steamapps/common/SteamVR
-
-if ((1<<32)); then
+if ((1 << 32)); then
   ARCH_TARGET=linux64
 else
   ARCH_TARGET=linux32
@@ -13,15 +10,18 @@ fi
 cmake . -DCMAKE_PREFIX_PATH=/opt/Qt/5.6/gcc_64/lib/cmake -DCMAKE_BUILD_TYPE=Release
 make -j4
 
+STEAM_PATH=~/.steam/steam
+STEAMVR_PATH=$STEAM_PATH/steamapps/common/SteamVR
+
 # Install
-rm -rf ~/.steam/steam/steamapps/common/SteamVR/drivers/sample
+rm -rf $STEAMVR_PATH/drivers/sample
 cp -r ./bin/drivers/sample $STEAMVR_PATH/drivers/sample
 mkdir $STEAMVR_PATH/drivers/sample/bin/${ARCH_TARGET}
 cp -r ./bin/${ARCH_TARGET} $STEAMVR_PATH/drivers/sample/bin/${ARCH_TARGET}
 
 # Cleanup
 rm CMakeCache.txt cmake_install.cmake Makefile
-rm driver_sample/cmake_install.cmake driver_sample/Makefile 
+rm driver_sample/cmake_install.cmake driver_sample/Makefile
 rm -rf CMakeFiles
 rm -rf driver_sample/CMakeFiles driver_sample/driver_sample_autogen
 rm -rf ./bin/${ARCH_TARGET}
